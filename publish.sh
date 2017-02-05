eb42bd737df6435e06db12e93ef83472ab574351
Sjaak Luthart <mail@sjaakluthart.nl>
2017-02-05 14:52:43 +0100

    Changed to npm build script.

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf build
-npm run build
-cp CNAME build/CNAME
-git add build
-git commit -m "Added new build"
-git push origin master
-git subtree push --prefix build origin gh-pages
