be236f9d1e94f92b253df12ee75dd75045e1c0ae
Sjaak Luthart <mail@sjaakluthart.nl>
2017-02-05 14:25:08 +0100

    Updated script.

--- publish.sh
+++ publish.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-rm -rf docs
 rm -rf build
 npm run build
-mv build docs
-cp CNAME docs/CNAME
-git add docs
-git commit -m "Published build"
+cp CNAME build/CNAME
+git add build
+git commit -m "Added new build"
 git push origin master
+git subtree push --prefix build origin gh-pages
