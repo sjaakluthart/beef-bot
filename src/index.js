7f6d7ce8a0cf4fc0cf3450f31335e017db6cbb4b
Sjaak Luthart <mail@sjaakluthart.nl>
2017-02-05 15:10:05 +0100

    Added emojione.ascii.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import emojione from 'emojione';
 import store from './store';
 import App from './components/app';
 import './index.css';
+
+emojione.ascii = true;
 
 ReactDOM.render(
   <Provider store={store}>
