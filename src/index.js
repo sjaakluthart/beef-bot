4dbec2b4bae80ad3ac14a96d902cb3575fc50083
Sjaak Luthart <mail@sjaakluthart.nl>
2017-07-01 19:56:48 +0200

    Changed to ThemeProvider, added service worker.

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import emojione from 'emojione';
-import WithTheme from 'anchor-ui/with-theme';
+import ThemeProvider from 'anchor-ui/theme-provider';
+import { StyleRoot } from 'radium';
+import registerServiceWorker from './registerServiceWorker';
 import store from './store';
 import App from './components/app';
 import './index.css';
 
 emojione.ascii = true;
+emojione.imagePathPNG = 'https://cdn.jsdelivr.net/emojione/assets/3.0/png/64/';
 
 ReactDOM.render(
-  <WithTheme color="#8991AF">
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </WithTheme>,
+  <StyleRoot style={{ height: '100%' }}>
+    <ThemeProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
+  </StyleRoot>,
   document.getElementById('root')
 );
+registerServiceWorker();
