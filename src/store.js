0e6d05039dd8e184e9dd9f0b8e68bfb04304da96
Sjaak Luthart <mail@sjaakluthart.nl>
2018-03-18 17:20:15 +0100

    Added thunk.

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 /* eslint no-underscore-dangle: ["error", { "allow": ["__REDUX_DEVTOOLS_EXTENSION__"] }] */
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import rootReducer from './reducers/root';
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(reduxThunk)
 );
 
 export default store;
