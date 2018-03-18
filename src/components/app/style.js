46ba43249dff9e17788c55c109894f0954714d70
Sjaak Luthart <mail@sjaakluthart.nl>
2018-03-18 17:44:06 +0100

    Changed mobile styles.

--- src/components/app/style.js
+++ src/components/app/style.js
@@ -9,10 +9,27 @@ export default () => ({
     boxShadow: styles.depthShadows[0]
   },
   input: {
-    width: 'calc(100% - 32px)'
+    width: '100%',
+    marginBottom: '0',
+    marginLeft: '0',
+    marginRight: '0',
+    boxShadow: 'none',
+
+    '@media(min-width: 768px)': {
+      width: 'calc(100% - 32px)',
+      marginBottom: '16px',
+      marginLeft: '16px',
+      marginRight: '16px',
+      borderRadius: '4px',
+      boxShadow: styles.depthShadows[0]
+    }
   },
   messages: {
-    height: 'calc(100% - 112px)'
+    height: 'calc(100% - 96px)',
+
+    '@media(min-width: 768px)': {
+      height: 'calc(100% - 112px)'
+    }
   },
   emojiMenu: {
     width: 'calc(100% - 32px)',
