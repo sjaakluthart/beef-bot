03fa70470a23b18ff02181132c294e95c4b943ef
Sjaak Luthart <mail@sjaakluthart.nl>
2020-12-06 09:14:53 +0100

    Updates

---
+++ precache-manifest.c6945b666133d7eb9ad0ecc069687f4f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb15a7eec9f8762cdb327b5895da06ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5584241844537c244c1c",
+    "url": "/static/css/main.c04d51b6.chunk.css"
+  },
+  {
+    "revision": "a297b617a50d46f7b375",
+    "url": "/static/js/2.fcca6463.chunk.js"
+  },
+  {
+    "revision": "b289205bee9d58262da3f724d0bb332f",
+    "url": "/static/js/2.fcca6463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5584241844537c244c1c",
+    "url": "/static/js/main.fdb81e2e.chunk.js"
+  },
+  {
+    "revision": "84b2b4ecbbc64144fe0b",
+    "url": "/static/js/runtime-main.42cff086.js"
+  },
+  {
+    "revision": "f58e467bbbf5a34614f05c2446781647",
+    "url": "/static/media/beef-bot.f58e467b.jpg"
+  },
+  {
+    "revision": "5a1593c0ba8453277dda6e1c23bc8481",
+    "url": "/static/media/send-accent.5a1593c0.svg"
+  },
+  {
+    "revision": "56bc1a620497fbfaf94f96ef2946f816",
+    "url": "/static/media/send.56bc1a62.svg"
+  }
+]);
